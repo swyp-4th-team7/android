b957e3ec478c7c9c476061109022bca312fa43d8
dodo <eg4rang@naver.com>
2026-03-11 18:30:02 +0900

    [Chore/#12] 앱 이름 변경

--- app/src/main/java/com/swyp/firsttodo/HeabomApplication.kt
+++ app/src/main/java/com/swyp/firsttodo/HeabomApplication.kt
@@ -6,7 +6,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class Application : Application() {
+class HeabomApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         setTimber()
