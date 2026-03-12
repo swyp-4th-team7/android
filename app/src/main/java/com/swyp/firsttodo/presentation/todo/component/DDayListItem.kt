d4bcdd5720053d7b1687a502bb7e2a80a5a558db
dodo <eg4rang@naver.com>
2026-03-12 23:22:04 +0900

    [Feat/#17] 컴포넌트 임시작업

---
+++ app/src/main/java/com/swyp/firsttodo/presentation/todo/component/DDayListItem.kt
@@ -0,0 +1,11 @@
+package com.swyp.firsttodo.presentation.todo.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DDayListItem(
+    dday: Int,
+    modifier: Modifier = Modifier,
+) {
+}
