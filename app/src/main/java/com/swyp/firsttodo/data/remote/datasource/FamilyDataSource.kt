2ad316bc2459150fbd3edd99df9eb4e96e26c2e0
dodo <eg4rang@naver.com>
2026-03-23 17:48:56 +0900

    [Feat/#42] family datasource 구현

---
+++ app/src/main/java/com/swyp/firsttodo/data/remote/datasource/FamilyDataSource.kt
@@ -0,0 +1,16 @@
+package com.swyp.firsttodo.data.remote.datasource
+
+import com.swyp.firsttodo.core.network.model.BaseResponse
+import com.swyp.firsttodo.data.remote.dto.request.family.FamilyConnectRequestBody
+import com.swyp.firsttodo.data.remote.dto.response.family.ConnectedFamilyResponseDto
+import com.swyp.firsttodo.data.remote.dto.response.family.FamilyDashboardResponseDto
+
+interface FamilyDataSource {
+    suspend fun postFamilyConnect(request: FamilyConnectRequestBody): BaseResponse<Unit>
+
+    suspend fun getConnectedFamily(): BaseResponse<ConnectedFamilyResponseDto>
+
+    suspend fun deleteFamilyConnect(targetUserId: Long): BaseResponse<Unit>
+
+    suspend fun getFamilyDashboard(): BaseResponse<FamilyDashboardResponseDto>
+}
