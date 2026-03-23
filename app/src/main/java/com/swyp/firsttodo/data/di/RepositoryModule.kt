9886164ea559b85ab7e9267a196c39d55876e6c0
dodo <eg4rang@naver.com>
2026-03-23 23:13:37 +0900

    [Feat/#45] schedule repository 구현

--- app/src/main/java/com/swyp/firsttodo/data/di/RepositoryModule.kt
+++ app/src/main/java/com/swyp/firsttodo/data/di/RepositoryModule.kt
@@ -3,11 +3,13 @@ package com.swyp.firsttodo.data.di
 import com.swyp.firsttodo.data.repositoryimpl.AuthRepositoryImpl
 import com.swyp.firsttodo.data.repositoryimpl.FamilyRepositoryImpl
 import com.swyp.firsttodo.data.repositoryimpl.NotificationRepositoryImpl
+import com.swyp.firsttodo.data.repositoryimpl.ScheduleRepositoryImpl
 import com.swyp.firsttodo.data.repositoryimpl.TodoRepositoryImpl
 import com.swyp.firsttodo.data.repositoryimpl.UserRepositoryImpl
 import com.swyp.firsttodo.domain.repository.AuthRepository
 import com.swyp.firsttodo.domain.repository.FamilyRepository
 import com.swyp.firsttodo.domain.repository.NotificationRepository
+import com.swyp.firsttodo.domain.repository.ScheduleRepository
 import com.swyp.firsttodo.domain.repository.TodoRepository
 import com.swyp.firsttodo.domain.repository.UserRepository
 import dagger.Binds
@@ -40,4 +42,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindsFamilyRepository(familyRepositoryImpl: FamilyRepositoryImpl): FamilyRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsScheduleRepository(scheduleRepositoryImpl: ScheduleRepositoryImpl): ScheduleRepository
 }
