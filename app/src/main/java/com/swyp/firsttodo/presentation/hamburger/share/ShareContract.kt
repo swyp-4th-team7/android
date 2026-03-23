a569c4060d8e8df3f698b01007bb56027d5b2727
dodo <eg4rang@naver.com>
2026-03-23 20:05:03 +0900

    [Feat/#42] 공유관리 ui 구현

--- app/src/main/java/com/swyp/firsttodo/presentation/hamburger/share/ShareContract.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/hamburger/share/ShareContract.kt
@@ -1,13 +1,21 @@
 package com.swyp.firsttodo.presentation.hamburger.share
 
 import androidx.compose.runtime.Immutable
+import com.swyp.firsttodo.core.base.Async
 import com.swyp.firsttodo.core.base.UiEffect
 import com.swyp.firsttodo.core.base.UiState
+import com.swyp.firsttodo.domain.model.family.ConnectedFamilyModel
 
 @Immutable
 data class ShareUiState(
-    val data: String = "",
-) : UiState
+    val codeErrorText: String? = null,
+    val inviteCode: Async<String> = Async.Init,
+    val families: Async<List<ConnectedFamilyModel>> = Async.Init,
+    val disconnectRequestMember: ConnectedFamilyModel? = null,
+) : UiState {
+    val showDialog = disconnectRequestMember != null
+    val dialogNickname = disconnectRequestMember?.nickname ?: ""
+}
 
 sealed interface ShareSideEffect : UiEffect {
     data class ShowSnackbar(val message: String) : ShareSideEffect
--- app/src/main/java/com/swyp/firsttodo/presentation/hamburger/share/ShareViewModel.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/hamburger/share/ShareViewModel.kt
@@ -1,10 +1,24 @@
 package com.swyp.firsttodo.presentation.hamburger.share
 
+import androidx.compose.foundation.text.input.TextFieldState
 import com.swyp.firsttodo.core.base.BaseViewModel
+import com.swyp.firsttodo.domain.model.family.ConnectedFamilyModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ShareViewModel
     @Inject
-    constructor() : BaseViewModel<ShareUiState, ShareSideEffect>(ShareUiState())
+    constructor() : BaseViewModel<ShareUiState, ShareSideEffect>(ShareUiState()) {
+        val codeFieldState = TextFieldState()
+
+        fun closeDialog() {}
+
+        fun disconnectFamily() {}
+
+        fun onInviteCodeInputDone() {}
+
+        fun onInviteCodeCopy() {}
+
+        fun onDisconnectClick(member: ConnectedFamilyModel) {}
+    }
