dded0db81e58d42c2748b0fe3c8ceb614f243437
dodo <eg4rang@naver.com>
2026-03-24 23:31:34 +0900

    [Feat/#53] reward service 구현

---
+++ app/src/main/java/com/swyp/firsttodo/data/remote/service/RewardService.kt
@@ -0,0 +1,28 @@
+package com.swyp.firsttodo.data.remote.service
+
+import com.swyp.firsttodo.core.network.model.BaseResponse
+import com.swyp.firsttodo.data.remote.dto.request.reward.RewardListResponseBody
+import com.swyp.firsttodo.data.remote.dto.request.reward.RewardRequestBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface RewardService {
+    @PATCH("/api/v1/habits/{habitId}/status/in-progress")
+    suspend fun patchInProgressHabit(
+        @Path("habitId") habitId: Long,
+        @Body request: RewardRequestBody,
+    ): BaseResponse<Unit>
+
+    @PATCH("/api/v1/habits/{habitId}/status/complete")
+    suspend fun patchCompleteHabit(
+        @Path("habitId") habitId: Long,
+    ): BaseResponse<Unit>
+
+    @GET("/api/v1/habits/rewards")
+    suspend fun getRewards(
+        @Query("status") status: String,
+    ): BaseResponse<RewardListResponseBody>
+}
