95973685994c5b666848a70ee48565d6eb9a846b
dodo <eg4rang@naver.com>
2026-03-25 15:21:36 +0900

    [Feat/#53] reward 서버 커스텀 에러처리 적용

--- app/src/main/java/com/swyp/firsttodo/data/repositoryimpl/RewardRepositoryImpl.kt
+++ app/src/main/java/com/swyp/firsttodo/data/repositoryimpl/RewardRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package com.swyp.firsttodo.data.repositoryimpl
 
+import com.swyp.firsttodo.core.network.model.ApiError
 import com.swyp.firsttodo.core.network.util.ApiResponseHandler
 import com.swyp.firsttodo.data.mapper.toModel
 import com.swyp.firsttodo.data.remote.datasource.RewardDataSource
 import com.swyp.firsttodo.data.remote.dto.request.reward.RewardRequestBody
 import com.swyp.firsttodo.domain.model.reward.RewardModel
 import com.swyp.firsttodo.domain.repository.RewardRepository
+import com.swyp.firsttodo.domain.throwable.RewardError
 import javax.inject.Inject
 
 class RewardRepositoryImpl
@@ -20,11 +22,34 @@ class RewardRepositoryImpl
         ): Result<Unit> =
             apiResponseHandler.safeApiCall {
                 rewardDataSource.patchInProgressHabit(habitId, RewardRequestBody(reward))
+            }.recoverCatching {
+                throw if (it is ApiError) {
+                    when (it.code) {
+                        40022 -> RewardError.RewardValueEmpty(it.serverMsg)
+                        40300 -> RewardError.AccessDenied(it.serverMsg)
+                        40401, 40405 -> RewardError.RewardNotFound(it.serverMsg)
+                        40026 -> RewardError.InvalidStatus(it.serverMsg)
+                        else -> it
+                    }
+                } else {
+                    it
+                }
             }
 
         override suspend fun completeReward(habitId: Long): Result<Unit> =
             apiResponseHandler.safeApiCall {
                 rewardDataSource.patchCompleteHabit(habitId)
+            }.recoverCatching {
+                throw if (it is ApiError) {
+                    when (it.code) {
+                        40300 -> RewardError.AccessDenied(it.serverMsg)
+                        40401, 40405 -> RewardError.RewardNotFound(it.serverMsg)
+                        40026 -> RewardError.InvalidStatus(it.serverMsg)
+                        else -> it
+                    }
+                } else {
+                    it
+                }
             }
 
         override suspend fun getRewards(status: String): Result<List<RewardModel>> =
