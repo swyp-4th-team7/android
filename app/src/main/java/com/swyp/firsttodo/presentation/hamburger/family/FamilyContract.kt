3bd8c0daf920416062a00cdfaa179f1dfc99f9aa
dodo <eg4rang@naver.com>
2026-03-25 19:27:04 +0900

    [Feat/#36] 가족보기 empty 뷰 추가

--- app/src/main/java/com/swyp/firsttodo/presentation/hamburger/family/FamilyContract.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/hamburger/family/FamilyContract.kt
@@ -4,12 +4,15 @@ import androidx.compose.runtime.Immutable
 import com.swyp.firsttodo.core.base.Async
 import com.swyp.firsttodo.core.base.UiEffect
 import com.swyp.firsttodo.core.base.UiState
+import com.swyp.firsttodo.core.common.extension.getDataOrNull
 import com.swyp.firsttodo.domain.model.family.FamilyInfo
 
 @Immutable
 data class FamilyUiState(
     val familyInfos: Async<List<FamilyInfo>> = Async.Init,
-) : UiState
+) : UiState {
+    val familyData = familyInfos.getDataOrNull()
+}
 
 sealed interface FamilySideEffect : UiEffect {
     data class ShowSnackbar(val message: String) : FamilySideEffect
