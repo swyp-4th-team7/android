e21aa77a2e9fef56cfd9b3c45de6e909857d1c37
dodo <eg4rang@naver.com>
2026-03-26 17:13:44 +0900

    [Fix/#36] todo 바텀시트 키보드 액션 변경 - as-is : next - to-be : done

--- app/src/main/java/com/swyp/firsttodo/presentation/todo/component/TodoBottomSheet.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/todo/component/TodoBottomSheet.kt
@@ -31,9 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -290,8 +288,6 @@ fun BottomSheetContent(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val focusManager = LocalFocusManager.current
-
     Column(modifier = modifier) {
         TaskSheetHeader(
             title = sheetType.title,
@@ -312,9 +308,8 @@ fun BottomSheetContent(
                     .fillMaxWidth()
                     .padding(bottom = 20.dp),
                 keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next,
+                    imeAction = ImeAction.Done,
                 ),
-                onKeyboardAction = { focusManager.moveFocus(FocusDirection.Next) },
             )
         }
 
