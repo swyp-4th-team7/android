8979399407f081da2473a83291fa9cdde89b6301
dodo <eg4rang@naver.com>
2026-03-26 15:05:20 +0900

    [Fix/#36] 스티커판 완료 팝업 UI 수정

--- app/src/main/java/com/swyp/firsttodo/presentation/reward/component/StickerBoardCompleteDialog.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/reward/component/StickerBoardCompleteDialog.kt
@@ -1,26 +1,31 @@
 package com.swyp.firsttodo.presentation.reward.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.swyp.firsttodo.R
+import com.swyp.firsttodo.core.common.extension.heightForScreenPercentage
 import com.swyp.firsttodo.core.common.extension.noRippleClickable
+import com.swyp.firsttodo.core.common.extension.widthForScreenPercentage
 import com.swyp.firsttodo.core.common.util.screenHeightDp
 import com.swyp.firsttodo.core.common.util.screenWidthDp
 import com.swyp.firsttodo.core.designsystem.component.HaebomBasicDialog
@@ -39,7 +44,6 @@ fun StickerBoardCompleteDialog(
     ) {
         Column(
             modifier = Modifier
-                .padding(horizontal = screenWidthDp(20.dp))
                 .padding(bottom = screenHeightDp(24.dp)),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
@@ -49,22 +53,28 @@ fun StickerBoardCompleteDialog(
                 modifier = Modifier
                     .align(Alignment.End)
                     .noRippleClickable(onDismiss)
-                    .padding(horizontal = 12.dp)
-                    .padding(top = 12.dp, bottom = 20.dp),
+                    .offset(x = screenWidthDp((-12).dp), y = screenWidthDp(12.dp)),
+                tint = Color.Unspecified,
             )
 
-            Box(
+            Spacer(Modifier.heightForScreenPercentage(28.dp))
+
+            Image(
+                painter = painterResource(R.drawable.img_scticker_complete_cheer),
+                contentDescription = null,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(120.dp)
-                    .padding(bottom = screenHeightDp(16.dp))
-                    .background(HaebomTheme.colors.gray300),
+                    .widthForScreenPercentage(120.dp)
+                    .heightForScreenPercentage(132.dp),
             )
+
+            Spacer(Modifier.heightForScreenPercentage(16.dp))
 
             Text(
                 text = "멋져요!\n30개의 스티커를 모았어요.",
                 modifier = Modifier
-                    .padding(bottom = screenHeightDp(28.dp)),
+                    .padding(vertical = 9.dp)
+                    .padding(horizontal = screenWidthDp(24.dp))
+                    .padding(bottom = screenHeightDp(24.dp)),
                 color = HaebomTheme.colors.black,
                 textAlign = TextAlign.Center,
                 style = HaebomTheme.typo.screen,
@@ -76,14 +86,13 @@ fun StickerBoardCompleteDialog(
                     .noRippleClickable(onCompleteBtnClick)
                     .fillMaxWidth()
                     .heightIn(screenHeightDp(40.dp))
-                    .padding(horizontal = screenWidthDp(6.dp))
+                    .padding(horizontal = screenWidthDp(24.dp))
                     .background(
                         color = HaebomTheme.colors.orange500,
                         shape = RoundedCornerShape(8.dp),
                     )
-                    .wrapContentHeight(Alignment.CenterVertically),
+                    .wrapContentSize(Alignment.Center),
                 color = HaebomTheme.colors.white,
-                textAlign = TextAlign.Center,
                 style = HaebomTheme.typo.section,
             )
         }
