aedbb3726fd20042fb5069c0bd785a6283ac1898
dodo <eg4rang@naver.com>
2026-03-27 17:55:13 +0900

    [Fix/#36] 로그인 ui 수정

--- app/src/main/java/com/swyp/firsttodo/presentation/auth/component/LegalLinks.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/auth/component/LegalLinks.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.swyp.firsttodo.core.common.extension.noRippleClickable
+import com.swyp.firsttodo.core.common.util.screenHeightDp
 import com.swyp.firsttodo.core.designsystem.theme.HaebomTheme
 import com.swyp.firsttodo.core.designsystem.theme.MediumStyle
 
@@ -24,7 +25,7 @@ fun LegalLinks(
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(4.dp),
+        verticalArrangement = Arrangement.spacedBy((screenHeightDp(4.dp))),
     ) {
         Text(
             text = "로그인하시면 아래 내용에 동의하는 것으로 간주됩니다.",
