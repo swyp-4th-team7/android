5a26d606570b0d85ae1c0bc94517d3eee3dec525
dodo <eg4rang@naver.com>
2026-03-30 20:51:17 +0900

    [Fix/#58] @OptIn 적용 - combinedClickable이 @ExperimentalFoundationApi라서 release 빌드에서 에러처리 됨

--- app/src/main/java/com/swyp/firsttodo/core/common/extension/ModifierExt.kt
+++ app/src/main/java/com/swyp/firsttodo/core/common/extension/ModifierExt.kt
@@ -1,5 +1,6 @@
 package com.swyp.firsttodo.core.common.extension
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
@@ -40,6 +41,7 @@ inline fun Modifier.noRippleClickable(
         }
     }
 
+@OptIn(ExperimentalFoundationApi::class)
 inline fun Modifier.noRippleLongClickable(
     crossinline onLongClick: () -> Unit,
     enabled: Boolean = true,
