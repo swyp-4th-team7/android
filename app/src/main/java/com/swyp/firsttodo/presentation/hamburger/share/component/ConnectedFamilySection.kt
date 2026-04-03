b4128377d9b2a608b8009b856df5bd7faee502b6
dodo <eg4rang@naver.com>
2026-04-03 22:50:06 +0900

    [Fix/#58] 중앙정렬 추가

--- app/src/main/java/com/swyp/firsttodo/presentation/hamburger/share/component/ConnectedFamilySection.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/hamburger/share/component/ConnectedFamilySection.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -66,12 +67,14 @@ private fun EmptyView(modifier: Modifier = Modifier) {
         Text(
             text = "아직 연결된 가족이 없어요.",
             color = HaebomTheme.colors.gray400,
+            textAlign = TextAlign.Center,
             style = HaebomTheme.typo.card,
         )
 
         Text(
             text = "초대코드를 입력하여 가족과 연결해 볼까요?",
             color = HaebomTheme.colors.gray300,
+            textAlign = TextAlign.Center,
             style = HaebomTheme.typo.description,
         )
     }
