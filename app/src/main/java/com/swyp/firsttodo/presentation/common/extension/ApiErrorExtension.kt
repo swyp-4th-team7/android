a12d2d6c1f03307c897ba2cd73c81ffa65a05c71
dodo <eg4rang@naver.com>
2026-04-03 10:00:45 +0900

    [Fix/#58] edit popup 롱프레스에서 클릭으로 변경

--- app/src/main/java/com/swyp/firsttodo/presentation/common/extension/ApiErrorExtension.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/common/extension/ApiErrorExtension.kt
@@ -9,7 +9,7 @@ fun ApiError.snackbarMsg(): String =
         is ApiError.Forbidden -> "접근 권한이 없어요."
         is ApiError.NotFound -> serverMsg
         is ApiError.Conflict -> serverMsg
-        is ApiError.ServerError -> "알 수 없는 에러가 발생했어요. 문제 상황이 지속되면 고객센터로 알려주세요."
+        is ApiError.ServerError -> "오류가 발생했어요! "
         is ApiError.NetworkConnection -> "네트워크 상태가 불안정해요. 다시 시도해주세요."
-        is ApiError.Unknown -> "알 수 없는 에러가 발생했어요. 문제 상황이 지속되면 고객센터로 알려주세요."
+        is ApiError.Unknown -> "오류가 발생했어요! "
     }
