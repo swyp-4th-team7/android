82ca705a30221d6ee184a794ea5a1b73d522ff7c
dodo <eg4rang@naver.com>
2026-04-03 17:03:49 +0900

    [Chore/#58] versionCode up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.swyp.firsttodo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
