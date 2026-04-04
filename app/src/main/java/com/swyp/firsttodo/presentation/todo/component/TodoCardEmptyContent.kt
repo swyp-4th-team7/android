4ad326c6c13f160922ae92110f66cc757abec873
dodo <eg4rang@naver.com>
2026-04-04 14:47:08 +0900

    [Fix/#58] 할 일 empty 뷰 아이콘 제거

--- app/src/main/java/com/swyp/firsttodo/presentation/todo/component/TodoCardEmptyContent.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/todo/component/TodoCardEmptyContent.kt
@@ -1,24 +1,18 @@
 package com.swyp.firsttodo.presentation.todo.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.swyp.firsttodo.R
 import com.swyp.firsttodo.core.designsystem.theme.HaebomTheme
 
 @Composable
@@ -26,7 +20,8 @@ fun TodoCardEmptyContent(
     text: String,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    Text(
+        text = text,
         modifier = modifier
             .fillMaxWidth()
             .heightIn(56.dp)
@@ -35,23 +30,11 @@ fun TodoCardEmptyContent(
                 shape = RoundedCornerShape(4.dp),
             )
             .padding(all = 16.dp)
-            .wrapContentWidth(Alignment.CenterHorizontally),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.Top,
-    ) {
-        Text(
-            text = text,
-            color = HaebomTheme.colors.gray400,
-            textAlign = TextAlign.Center,
-            style = HaebomTheme.typo.subtitle,
-        )
-
-        Icon(
-            imageVector = ImageVector.vectorResource(R.drawable.ic_curved_arrow),
-            contentDescription = null,
-            tint = HaebomTheme.colors.gray400,
-        )
-    }
+            .wrapContentSize(Alignment.Center),
+        color = HaebomTheme.colors.gray400,
+        textAlign = TextAlign.Center,
+        style = HaebomTheme.typo.subtitle,
+    )
 }
 
 @Preview
