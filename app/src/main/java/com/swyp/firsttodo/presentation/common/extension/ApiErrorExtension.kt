5c4c1cc082b18edb16551e39c7fee06b5b581d20
dodo <eg4rang@naver.com>
2026-04-05 18:16:05 +0900

    [Fix/#58] 띄어쓰기 수정

--- app/src/main/java/com/swyp/firsttodo/presentation/common/extension/ApiErrorExtension.kt
+++ app/src/main/java/com/swyp/firsttodo/presentation/common/extension/ApiErrorExtension.kt
@@ -9,7 +9,7 @@ fun ApiError.snackbarMsg(): String =
         is ApiError.Forbidden -> "접근 권한이 없어요."
         is ApiError.NotFound -> serverMsg
         is ApiError.Conflict -> serverMsg
-        is ApiError.ServerError -> "오류가 발생했어요! "
+        is ApiError.ServerError -> "오류가 발생했어요!"
         is ApiError.NetworkConnection -> "네트워크 상태가 불안정해요. 다시 시도해 주세요."
-        is ApiError.Unknown -> "오류가 발생했어요! "
+        is ApiError.Unknown -> "오류가 발생했어요!"
     }
