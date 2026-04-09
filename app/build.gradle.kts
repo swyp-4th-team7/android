b0ba26611973eb439a28997804150454baf07ed4
dodo <108921606+doyeon0307@users.noreply.github.com>
2026-04-09 18:36:03 +0900

    Feat/#71 todo empty banner (#72)
    * [Feat/#71] todo empty 배너 노출
    
    * [Chore/#71] versionCode up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.swyp.firsttodo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.0.1"
+        versionCode = 13
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
