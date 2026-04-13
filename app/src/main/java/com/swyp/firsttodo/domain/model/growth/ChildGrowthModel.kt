fdffbbf7be667e4a217ba51b106e360de0360047
dodo <108921606+doyeon0307@users.noreply.github.com>
2026-04-13 23:49:18 +0900

    [Feat/#77] 성장탭 삭제 및 다가오는 일정 복구 (#78)
    * [Chore/#77] 성장탭 삭제
    
    * [Feat/#77] 스케줄 복구

--- app/src/main/java/com/swyp/firsttodo/domain/model/growth/ChildGrowthModel.kt
+++
@@ -1,9 +0,0 @@
-package com.swyp.firsttodo.domain.model.growth
-
-data class ChildGrowthModel(
-    val childId: Long,
-    val nickname: String,
-    val todoStarCount: Int,
-    val habitStarCount: Int,
-    val weekRange: String,
-)
